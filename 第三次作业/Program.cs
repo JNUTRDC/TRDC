3592daa5fd8b97c4634c5d7d0439544e98c57966
timetraver <32676489+timetraver@users.noreply.github.com>
2017-11-14 12:31:22 +0800

    赵乙璇翻纸牌游戏（无限bug版）

--- 第三次作业/Program.cs
+++ 第三次作业/Program.cs
@@ -2,153 +2,195 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace game2
+namespace 纸牌游戏
 {
     class Program
     {
-        public static int[] rdom = new int[16];
+        public static int[] math = new int[16];
         public static int[] face = new int[16];
-        public static int[] condition = new int[16];
-        public static int[] right = new int[16];
-        static void Start1()
+        public static int[] now = new int[16];
+        public static int[] ou = new int[16];
+        public static string fal;
+        public static string life;
+
+        static void Start()
         {
-            //顺序赋值
-            for (int i = 0; i <= 7; i++)
+            for (int i = 0; i <= 3; i++)
             {
-                for(int j = 0;j <= 1; j++)
+                for (int j = 0; j <= 3; j++)
                 {
-                    rdom[2 * i + j] = i + 1;
+                    math[i * 2 + j] = i + 1;
                 }
             }
-            //赋值结束
-        }//编码
-        static void Start2()
-        {
-            int length = 15,t;
-            Random rd = new Random();
+            int L = 15, x;
+            Random go = new Random();
             for (int i = 0; i <= 15; i++)
             {
-                t = rd.Next(0, length);
-                face[i] = rdom[t];
-                rdom[t] = rdom[length];
-                length -= 1;   
-            }
-        }//赋值
-        static void Print()
-        {
-            Console.Clear();
-            for (int i = 0; i < 16; i++)
-            {
-                if (condition[i] == 0)
-                    Console.Write(" * ");
-                else
-                    Console.Write(" "+face[i]+" ");
-                if ((i+1) % 4 == 0)
-                    Console.WriteLine();
-            }
-        }//打印
-        static void Game()
-        {
-            int Line1, Colomn1,Line2,Colomn2,bingo=0;
-            for (int i = 0; i < 16; i++)
-            { condition[i] = 1; }
-            Print();
-            for (int i = 0; i < 16; i++)
-            { condition[i] = 0; }
-            Console.WriteLine("你还有3秒钟观察时间");
-            Thread.Sleep(1000);
-            Console.WriteLine("你还有2秒钟观察时间");
-            Thread.Sleep(1000);
-            Console.WriteLine("你还有1秒钟观察时间");
-            Thread.Sleep(1000);
-            for (; ; )
-            {
-                Print();
-                for (; ; )
-                {
-                    Console.Write("请输入第一个坐标的行:");
-                    Line1 = Convert.ToInt16(Console.ReadLine());
-                    if (0 >= Line1 && Line1 >=5) Console.WriteLine("输入错误！");
-                    else break;
-                }               
-                for (; ; )
-                {
-                    Console.Write("请输入第一个坐标的列:");
-                    Colomn1 = Convert.ToInt16(Console.ReadLine());
-                    if (0 >= Colomn1 && Colomn1 >= 5) Console.WriteLine("输入错误！");
-                    else break;
-                }
-                condition[(Line1 - 1) * 4 + Colomn1 - 1] = 1;
-                Print();               
-                for (; ; )
-                {
-                    Console.Write("请输入第二个坐标行");
-                    Line2 = Convert.ToInt16(Console.ReadLine());
-                    if (0 >= Line2 && Line2 >= 5) Console.WriteLine("输入错误！");
-                    else break;
-                }
-                for (; ; )
-                {
-                    Console.Write("请输入第二个坐标列");
-                    Colomn2 = Convert.ToInt16(Console.ReadLine());
-                    if (0 >= Colomn2 && Colomn2 >= 5) Console.WriteLine("输入错误！");
-                    else break;
-                }
-                condition[(Line2 - 1) * 4 + Colomn2 - 1] = 1;
-                Print();
-                if (face[(Line1-1)*4+Colomn1 -1]== face[(Line2 - 1) * 4 + Colomn2 - 1]
-                    && right[(Line1 - 1) * 4 + Colomn1 - 1] == 0
-                    && right[(Line2 - 1) * 4 + Colomn2 - 1] == 0
-                   )
-                {
-                    Console.WriteLine("对了！");
-                    condition[(Line1 - 1) * 4 + Colomn1 - 1] = condition[(Line2 - 1) * 4 + Colomn2 - 1] = 1;
-                    right[(Line1 - 1) * 4 + Colomn1 - 1] = right[(Line2 - 1) * 4 + Colomn2 - 1] = 1; Thread.Sleep(1000);
-                }
-                else if (right[(Line1 - 1) * 4 + Colomn1 - 1] != 0 || right[(Line2 - 1) * 4 + Colomn2 - 1] != 0)
-                {
-                    Console.WriteLine("操作错误，请重输一遍！");
-                    if (right[(Line1 - 1) * 4 + Colomn1 - 1] == 0) condition[(Line1 - 1) * 4 + Colomn1 - 1] = 0;
-                    if (right[(Line2 - 1) * 4 + Colomn2 - 1]==0)condition[(Line2 - 1) * 4 + Colomn2 - 1]=0;
-                    Thread.Sleep(1000);
-                }
-                else
-                {
-                    Print();
-                    Console.WriteLine("错了！");
-                    condition[(Line1 - 1) * 4 + Colomn1 - 1] = condition[(Line2 - 1) * 4 + Colomn2 - 1] = 0;
-                    Thread.Sleep(1000);
-                }
-                for (int i = 0; i < 16; i++)
-                {
-                    if (condition[i] == 1) bingo += 1;
-                }
-                if (bingo == 16)
-                {
-                    Console.WriteLine("您胜利了！！！！！！");
-                    break;
-                }
-                else bingo = 0;
+                x = go.Next(0, L);
+                face[i] = math[x];
+                math[x] = math[L];
+                L = 1;
             }
 
-        }//主游戏
+        }
+        static void Cardback()
+        {
+            Console.Clear();
+            for (int i = 0; i <= 15; i++)
+            {
+                if (now[i] == 0)
+                    Console.WriteLine("*");
+                else
+                    Console.WriteLine("{face[i]}");
+                if ((i + 1) % 16 == 0)
+                    Console.WriteLine();
+
+            }
+        }
+        static void Welcome()
+        {
+            string Player;
+            Console.WriteLine("欢迎进入游戏，请问您的名字是？");
+            Player = Console.ReadLine();
+            Console.WriteLine($"欢迎你！挑战者{Player}！\n");
+            Thread.Sleep(1000);
+            Console.WriteLine("游戏规则");
+            Console.WriteLine("4X4超简易翻纸牌益智小游戏");
+            Console.WriteLine("按照游戏提示输入（可以尝试输入其他的数字哦，有彩蛋）\n当全部牌翻开或生命值耗尽时结束游戏。\n");
+            Thread.Sleep(1000);
+        }
+
+        public static void Game()
+        {
+            int h1, h2, r1, r2, v;
+            
+            for (int i = 0; i <= 15; i++)
+            {
+                now[i] = 0;
+                Cardback();
+            }
+            Console.WriteLine("即将开始游戏，您拥有五秒的观察时间，请集中╭( ･ㅂ･)و ");
+            Thread.Sleep(5000);
+            Console.WriteLine("Start!");
+            for (; ; )
+            {
+                Console.WriteLine("请输入您要翻的第一个牌子的行~");
+                h1 = Convert.ToInt16(Console.ReadLine());
+                if (h1 >= 5)
+                {
+                    Console.WriteLine("您输入的行数已经溢出去了╰(￣▽￣)╭");
+                    continue;
+                }
+
+                if (h1 == 0)
+                {
+                    Console.WriteLine("......0什么的、不存在的");
+                    continue;
+                }
+                else break;
+            }
+            
+            for (; ; )
+            {
+                Console.WriteLine("请输入您要翻的第一个牌子的列~");
+                r1 = Convert.ToInt16(Console.ReadLine());
+                if (r1 >= 5)
+                    Console.WriteLine("我很好奇您是怎么到这么一列的，明明这么明显");
+                if (r1 == 0)
+                    Console.WriteLine("哈哈哈，别想了，没有这有这一列的.....");
+                else break;
+
+            }
+            now[(h1 - 1) * 4 + r1 - 1] = 1;
+            Cardback();
+            for (; ; )
+            {
+                Console.WriteLine("请输入您要翻的第二个牌子的行~");
+                h2 = Convert.ToInt16(Console.ReadLine());
+                if (h2 >= 5)
+                    Console.WriteLine("这个是........4X4翻纸牌游戏哦......重新输入");
+                if (h2 == 0)
+                    Console.WriteLine("能输入这种数字，我也是很佩服你的= =");
+                else break;
+
+            }
+            for (; ; )
+            {
+                Console.WriteLine("请输入您要翻的第二个牌子的列~");
+                r2 = Convert.ToInt16(Console.ReadLine());
+                if (r2 >= 5)
+                    Console.WriteLine($"喵、并没有{r2}这种东西的存在，请看着规则重新输入（微笑）");
+                if (r2 == 0)
+                    Console.WriteLine("我应该从哪里开始吐槽呢....");
+            }
+            now[(h2 - 1) * 4 + r2 - 1] = 1;
+            Cardback();
+            if (face[(h1 - 1) * 4 + r1 - 1] == face[(h2 - 1) * 4 + r2 - 1] &&
+                ou[(h1 - 1) * 4 + r1 - 1] == 0 && ou[(h2 - 1) * 4 + r2 - 1] == 0)
+            {
+                Console.WriteLine("成功！记忆力超强的！");
+                now[(h1 - 1) * 4 + r1 - 1] = now[(h2 - 1) * 4 + r2 - 1] = 1;
+                ou[(h1 - 1) * 4 + r1 - 1] = ou[(h2 - 1) * 4 + r2 - 1] = 1;
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Thread.Sleep(1000);
+                Cardback();
+                Console.WriteLine("失败(ﾉ*･ω･)ﾉ，加油！");
+                now[(h1 - 1) * 4 + r1 - 1] = now[(h2 - 1) * 4 + r2 - 1] = 0;
+                int life = Convert.ToInt32(life);
+                life-=1;
+                Thread.Sleep(1000);
+            }
+            for (int i = 0; ; i++)
+            {
+                if (now[i] == 1)
+                {
+                    v += 1;
+                }
+                if (v == 16)
+                {
+                    Console.WriteLine("恭喜你翻开了所有的牌！大吉大利，晚上吃鸡！");
+                    break;
+                }
+                else
+                    v = 0;
+            }
+
+        }
+        public static void Choose()
+        {
+            Console.WriteLine("勇敢的挑战者，请输入你希望上天给予的生命值");
+            life = Console.ReadLine();
+
+            Console.WriteLine($"以生命值——{life}开始游戏\n按任意键继续");
+        }
+        public static void Result()
+        {
+            for (; ; )
+            {
+                int m = Convert.ToInt32(life);
+                if (m == 0)
+                {
+                    Console.WriteLine("bia！你死了！");
+                    return;
+                }
+                else break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("******************");
-            Console.WriteLine("本游戏为翻纸牌游戏");
-            Console.WriteLine("   开局三秒观察 ");
-            Console.WriteLine("然后翻出数字相同的牌");
-            Console.WriteLine("翻出全部牌则为胜利");
-            Console.WriteLine("******************");
-            Console.WriteLine("ps：由于我技术不佳的原因，请不要连续敲击回车键，否则引起崩溃，谢谢合作！");
-            Console.WriteLine("按任意键继续。。。");
+            Welcome();
+            Choose();
             Console.ReadKey();
-            Start1();
-            Start2();
+            Start();
             Game();
+            Result();
         }
+
     }
 }
